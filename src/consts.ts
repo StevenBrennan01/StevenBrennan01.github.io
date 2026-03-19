bbf836eebd9bb7536ccd1fa184cdaf0c3cbef8dd
StevenBrennan01 <steveb1701@outlook.com>
2026-03-19 11:24:57 +0000

    Detail tweaks

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Ataraxia Astro Theme';
-export const SITE_DESCRIPTION = 'Ataraxia is a minimal, accessible Astro v6 theme for building fast, content-focused websites.';
-export const SITE_AUTHOR = 'Your Name Here';
+export const SITE_TITLE = 'Steven Brennan | GameDev Portfolio';
+export const SITE_DESCRIPTION = 'Steven Brennan\'s Portfolio showcasing game development projects and skills.';
+export const SITE_AUTHOR = 'Steven Brennan';
